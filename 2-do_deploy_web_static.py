8f422da220a4619d68cf01920230a6eeaaaa710a
Kwesi20000 <hendrickskumi@yahoo.com>
2024-02-01 10:04:49 -0800

    hendy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.237.79.224', '54.90.9.35']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
