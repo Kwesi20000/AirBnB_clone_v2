7f34c838aff4286212d86927a0f779fd1f59bc16
Kwesi20000 <hendrickskumi@yahoo.com>
2024-02-16 12:51:45 -0800

    hendy

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This script starts a Flask web application.
-"""
+"""script that starts a Flask web application"""
 
 from models import storage
 from flask import Flask
@@ -12,14 +10,12 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def state_list():
-    """Display a list of all State objects present in DBStorage"""
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
